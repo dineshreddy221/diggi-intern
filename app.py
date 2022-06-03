25a32029e468ae84fe36926b826565900fe13fb7
dineshreddy221 <uppuluri007i@gmail.com>
2022-06-03 09:43:41 +0530

    added page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home_get():
     return render_template("index.html")
 
+@app.route('/feature')
+def features():
+    return render_template('feature.html')
+
 if __name__ =='__main__':
     app.run(debug=True)
