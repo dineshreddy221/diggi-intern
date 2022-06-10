c610881f64c854673b27f4d0db2cf87b1bb44b76
dineshreddy221 <uppuluri007i@gmail.com>
2022-06-10 13:52:43 +0530

    added new lines of code with different modes

--- file_handling.py
+++ file_handling.py
@@ -1,19 +1,49 @@
+# opening the file and closing after print.
 file = open("a.txt")
 text = file.read()
 print(text)
 file.close()
 
 
-# opening file with statement
+# opening file in read mode using with
 with open("b.txt") as f:
-    f_read = f.read()
+    f_read = f.read()  # it reads the files and gives us the text from file
     print(f_read)
 
-# try and except
+# Opening a file in write mode
+with open("b.txt", mode="w") as open_write:  # whenever we open file in write mode all the data is lost.
+    context = open_write.write("dinesh")   # this line every time writes something new after clearing.
+    print(context)
 
-try:
-    n = input("enter a number\n")
-    a = n + 4
-    print(a)
-except TypeError:
-    print("type error")
+
+# opening the file in append mode
+with open("b.txt", mode="a") as append_mode:
+    text = append_mode.write("\nhi, welcome") # it writes a new line and append in the file
+    print(text)
+
+# checking read mode functions
+with open("b.txt", mode="r") as opn_read:
+    text = opn_read.read()         # this read method print the text in the file.
+    print(text)
+
+with open("b.txt") as opn_read:
+    text = opn_read.readlines()  # this reads all the file and give that in a list.
+    print(text)
+    print(type(text))
+
+with open("b.txt") as opn_read:
+    text = opn_read.readable()  # this reads all the file and give that in a list.
+    print(text)
+    print(type(text))
+
+
+with open("b.txt") as opn_read:
+    text = opn_read.readline()  # this reads only the first line of the file.
+    print(text)
+    print(type(text))
+
+
+# writing a new lines using append mode.
+with open("b.txt", mode="a") as opn_read:
+    text = opn_read.writelines("\nwriting a new lines with append mode\nthe lines has appended")
+    print(text)
