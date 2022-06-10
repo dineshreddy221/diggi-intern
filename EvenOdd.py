69d03ffab77a950297d1b2bb48efa80a30b4b1fa
dineshreddy221 <uppuluri007i@gmail.com>
2022-06-10 11:03:07 +0530

    adding new elif

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,19 +1,22 @@
-# defing a fuction for even odd numbers.
-def even_odd(num):
-    if num%2==0:
+
+# Taking the number input from user.
+num = int(input("enter a number\n"))
+
+
+# defining a function for even odd numbers.
+def even_odd():
+    global num
+    if num % 2 == 0:
         print("Even Number : {}".format(num))
-        if num>10:
+        if 10 <= num <= 99:
             print(f"It is a even two digit number : {num}")
+        elif 99 < num <= 999:
+            print(f"It is even three digit number : {num}")
         else:
             print(f"It is even single digit number : {num}")
     else:
         print("Odd Number : {}".format(num))
 
 
-# Taking the number as input from user.
-num = int(input())
-
 # calling the function
-even_odd(num)
-
-
+even_odd()
