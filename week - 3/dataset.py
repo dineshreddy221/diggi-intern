8f8d6890b47e79e69bd1b296e5b0a81f732b58e0
dineshreddy221 <uppuluri007i@gmail.com>
2022-06-17 18:15:35 +0530

    created a dataframe and done some data cleaning and manipulation.

--- week - 3/dataset.py
+++ week - 3/dataset.py
@@ -3,27 +3,27 @@ import pandas as pd
 # import numpy as np
 
 df = pd.read_csv(r"C:\Users\Lakshman\Downloads\ML & DL\dataset\Churn_Modelling.csv")
-print(df.head())
+print(df.head(), end="\n")
 
 # checking the shape of a dataset
-print(df.shape)
+print(df.shape, end="\n\n")
 
 # checking information
-print(df.info())
+print(df.info(), end="\n\n")
 
 # describe gives us descriptive statistical information for numerical variable
-print(df.describe())
+print(df.describe(), end='\n\n')
 
 # describe for categorical variables
 df_desc_cat = df.describe(include=['object'])
-print(df_desc_cat)
+print(df_desc_cat, end='\n\n')
 
 # checking for null values
 df_null = df.isnull().sum()
-print(df_null)  # there are no null values in the dataset
+print(df_null, end='\n\n')  # there are no null values in the dataset
 
 # this gives us the unique values
-print(df['Gender'].unique())
+print(df['Gender'].unique(), end='\n\n')
 
 # finding the surname of male using geography as spain.
 df_surname = df['Surname'][(df['Gender'] == 'Male') & (df['Geography'] == 'Spain')]
