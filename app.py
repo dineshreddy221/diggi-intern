3c03e977964d15a9ed8e35e03fa07a1faecd5b34
dineshreddy221 <uppuluri007i@gmail.com>
2022-06-29 10:45:49 +0530

    added new lines and updated

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_get():
     return render_template("index.html")
+
 
 @app.route('/feature')
 def features():
     return render_template('feature.html')
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
